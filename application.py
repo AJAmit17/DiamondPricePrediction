9a8bc3f5dd75539e6b342787d3e08ff16f91596d
AJAmit17 <amitjagadeesh2004@gmail.com>
2023-09-25 00:03:42 +0530

    'templatesUpdate'

--- application.py
+++ application.py
@@ -1,16 +1,11 @@
 from flask import Flask,request,render_template,jsonify
 from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
-
 application=Flask(__name__)
 
 app=application
 
-@app.route('/')
-def home_page():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('form.html')
